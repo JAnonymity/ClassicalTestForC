610bd0e397249b43adeabe3fcead10b0d24d8b11
JAnonymity <68265498+JAnonymity@users.noreply.github.com>
2021-10-22 10:56:56 +0800

    Create 7.PrintPatterns.c

---
+++ 7.PrintPatterns.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+//7.打印以下图案：
+//   *
+//  ***
+// *****
+//*******
+void main()
+{
+	int rows=0,spaces=0,stars=0;
+	 
+	for(rows=1;rows<=4;rows++)
+	{
+		for(spaces=0;spaces<4-rows;spaces++)
+		{
+			printf(" ");
+		}
+		
+		for(stars=0;stars<2*rows-1;stars++)
+		{
+			printf("*");
+		}
+		
+		printf("\n");
+	}
+}
